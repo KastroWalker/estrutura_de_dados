bf859190857b5def8980465c26a03220e63b3ee7
kastrowalker <victorcsa2002@gmail.com>
2021-01-24 23:44:56 -0300

    Criando ordenaçao utilizando selection sort

---
+++ sort.py
@@ -0,0 +1,16 @@
+array = [-2, -1, 3, 4, 8, 50, 60, 37, 12, 11, 36, 70]
+
+def my_sort(array):
+    for index in range(0, len(array)):    
+        smaller = array[index]
+        
+        for number in array[index:]:
+            if number < smaller:
+                smaller = number
+            
+        array.insert(index, array.pop(array.index(smaller)))
+            
+    return array
+
+orded_array = my_sort(array)
+print(orded_array)
